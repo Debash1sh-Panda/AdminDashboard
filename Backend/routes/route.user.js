81610466f1232da976f614977fa2b383bec11742
Debash1sh-Panda <debasishpanda2036@gmail.com>
2024-09-03 18:55:29 +0530

    add

--- Backend/routes/route.user.js
+++ Backend/routes/route.user.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 const { register, login, getAllUsers, logout } = require('../controllers/controller.user.js');
+const isAuthenticated = require('../middleware/isAuthenticated.js');
 
 router.post('/register', register);
 router.post('/login', login);
 router.get('/logout', logout);
-router.get('/get-all-users', getAllUsers);
+router.get('/get-all-users',isAuthenticated, getAllUsers);
 
 module.exports = router;
